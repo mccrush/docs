dd763991c8fd166ab55bdc4eb1a060de148c586e
Sergey <mccrush2027@gmail.com>
2026-09-20 15:05:37 +0000

    Update deploy

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   srcDir: "docs",
+  base: '/docs/',
   
   title: "mccrush.ru/docs",
   description: "Документация по разработке ПО",
